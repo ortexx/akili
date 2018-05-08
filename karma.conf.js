4325397a3850931ce2e3afb6ef6ddcced5904d5d
Alexandr Balasyan <mywebstreet@gmail.com>
2018-05-08 19:10:30 +0300

    added preset to tests

--- karma.conf.js
+++ karma.conf.js
@@ -45,7 +45,7 @@ module.exports = function(config) {
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
-              presets: ['env', 'stage-2', 'stage-3']
+              presets: ['akili']
             }
           }
         ]
