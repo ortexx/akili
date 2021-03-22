e5fe0c46d5b9274e0aca3ac515fd18b33a80fc77
Alexandr Balasyan <mywebstreet@gmail.com>
2021-03-22 09:40:54 +0300

    upgraded deps

--- dist/akili.js
+++ dist/akili.js
@@ -3,7 +3,7 @@
  * 
  * const Akili = makeItEasy(js + html);
  * 
- * @version 1.2.10
+ * @version 1.2.11
  * @author Alexandr Balasyan <mywebstreet@gmail.com>
  * {@link http://akilijs.com}
  * {@link https://github.com/ortexx/akili}
